e304cd0bded3a2b2897fec73ef0061a34f79ebff
Klayder <anderysorokin@gmail.com>
2018-04-12 14:11:18 +0300

    implement all section; slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
 @@include("../../bower_components/jquery/dist/jquery.min.js");
+@@include("slick.js";)
 
 
 $(document).ready( function(){
   console.log("Hi! JQuery work!");
+  $('.mainContent__slider').slick({
+  	infinite: true,
+  	dots: true,
+  	speed: 600,
+  	adaptiveHeight: true,
+  	autoplay: true,
+  	autoplaySpeed: 3500
+    
+  });
 });
