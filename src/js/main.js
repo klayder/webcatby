3a1e766c067f563dc97008d491a7727d02d33554
Klayder <anderysorokin@gmail.com>
2018-04-12 17:03:05 +0300

    add popup

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 
 $(document).ready( function(){
   console.log("Hi! JQuery work!");
+  
   $('.mainContent__slider').slick({
   	infinite: true,
   	dots: true,
@@ -13,4 +14,31 @@ $(document).ready( function(){
   	autoplaySpeed: 3500
     
   });
+  
+let modalOverlay=$(".modalOverlay");
+
+$(".mainContent_priceFormBtn").click((evt)=>{
+		evt.preventDefault();
+		console.log("click");
+		modalOverlay.addClass("modalOverlay--show");
+});
+
+
+modalOverlay.click(hideOverlay);
+
+$(window).keyup((evt)=>{
+		if(evt.keyCode===27){
+			if(modalOverlay.hasClass("modalOverlay--show")){
+				console.log("ok");
+				hideOverlay();
+			}
+		}
+});
+
+
+function hideOverlay(){
+	modalOverlay.removeClass("modalOverlay--show");
+}
+
+
 });
