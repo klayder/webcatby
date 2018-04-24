4b5adced7069d322807de5949e2c4a072a4cc5a6
Andrey Sorokin <anderysorokin@gmail.com>
2018-04-24 13:51:01 +0300

    fix section header

--- belSlavGarant/src/js/main.js
+++ belSlavGarant/src/js/main.js
@@ -1,9 +1,11 @@
 @@include("../../bower_components/jquery/dist/jquery.min.js");
-
+@@include("../../node_modules/nicescroll/dist/jquery.nicescroll.min.js");
 
 
 $(document).ready( function(){
   console.log("Hi! JQuery work!");
+  $("body").niceScroll();
+  $(".mainContent__textReviwWrap article").niceScroll();
   
   
 });
